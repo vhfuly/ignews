2a825bf1d33a8e00341d912dedd870eb327c4119
vhfuly <victor_hugo_fuly@hotmail.com>
2021-12-13 18:24:26 -0300

    feat: create page home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,7 @@ export function Header() {
     return(
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <p>ig.news</p>
+                <img src="/images/logo.svg" alt="ig.news" /> 
                 <nav>
                     <a className={styles.active}>Home</a>
                     <a>Posts</a>
